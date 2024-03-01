38852b90cf202b4ff4f0bedb5a8c802825156814
Salvatore <mammoliti.sam@gmail.com>
2024-03-01 10:14:22 -0500

    Create models for Post and User entities and implemented the Post and User models with defined attributes and validations in seperate files

---
+++ models/index.js
@@ -0,0 +1,39 @@
+// const User = require('./User');
+// const Post = require('./Post');
+// const Comment = require('./Comment');
+
+// User.h
+
+
+
+// A User can have many Posts.
+
+// A User can have many Comments.
+
+
+
+// Post Model:
+
+
+
+// A Post belongs to a User.
+
+// A Post can have many Comments.
+
+
+
+// Comment Model:
+
+
+
+// A Comment belongs to a User.
+
+// A Comment belongs to a Post.
+
+
+
+
+// These associations are established using Sequelize associations like belongsTo, hasMany, or hasOne depending on the relationship between the models.
+
+
+
---
+++ models/user.js
@@ -0,0 +1,73 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [9, 87],
+        notEmpty: true,
+        isComplex(value) {
+          if (!/[a-zA-Z]/.test(value)) {
+            throw new Error('Password must contain at least one letter');
+          }
+          if (!/[0-9]/.test(value)) {
+            throw new Error('Password must contain at least one number');
+          }
+          if (!/[^a-zA-Z0-9]/.test(value)) {
+            throw new Error('Password must contain at least one special character');
+          }
+        }
+      }
+    }
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10,
+        );
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  },
+);
+
+module.exports = User;
